3d64e38ed0e5cb43fa5aa6d9b3fb71183ac7de17
dursunkatar <dursun.katar@gmail.com>
2025-03-25 21:34:59 +0300

    no message

---
+++ OrderManagementSystem/OMS.Infrastructure/Persistence/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using OMS.Application.Interfaces;
+using OMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Infrastructure.Persistence
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderDbContext _context;
+
+        public Task<Order> AddAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetByCustomerIdAsync(int customerId, int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
