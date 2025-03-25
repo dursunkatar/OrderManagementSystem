d552e6d01e999365b393b59eab63f09dca2114b0
dursunkatar <dursun.katar@gmail.com>
2025-03-25 21:50:23 +0300

    no message

---
+++ OrderManagementSystem/OMS.Infrastructure/Messaging/RabbitMQEventPublisher.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using OMS.Application.Interfaces;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Infrastructure.Messaging
+{
+    public class RabbitMQEventPublisher : IEventPublisher
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public Task PublishAsync<T>(T @event) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        // Implementasyon...
+    }
+}
